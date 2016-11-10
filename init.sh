02af2f5654855c62349cc4d00b2a0ad1cee37dc5
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2016-11-10 18:59:01 +0700

    init.sh updated

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
 ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-gunicorn -b '0.0.0.0:8080' /home/box/web/hello.py:hello
+/etc/init.d/gunicorn restart
